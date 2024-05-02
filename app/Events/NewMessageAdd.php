9dd7c4fbc476abca9765e18987672ca7d2033ea9
Ahmed Soliman <ahmed.abdelazim911@gmail.com>
2024-05-02 21:58:13 +0300

    update event mess

--- app/Events/NewMessageAdd.php
+++ app/Events/NewMessageAdd.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Http\Resources\ChatRoomMessageResource;
 use App\Models\Message;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
@@ -34,12 +35,9 @@ class NewMessageAdd implements ShouldBroadcastNow
         return 'message.sent';
     }
 
-    public function broadcastWith(): array
+    public function broadcastWith(): ChatRoomMessageResource
     {
-        return [
-            'chat_id' => $this->chatMessage->room_id,
-            'message' => $this->chatMessage->content,
-        ];
+        return new ChatRoomMessageResource($this->chatMessage);
     }
     /**
      * Get the channels the event should broadcast on.
